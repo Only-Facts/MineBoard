2a3fb5e67f7debd185fd3cf4ecc7db6d1a534f83
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-11-24 17:21:34 +0100

    [UPDATE] main.rs
    Added AppState struct, actix-web, "/api/start" & "/api/stop routes" &
    front in "/"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Mutex;
+
+use actix_files::{Files, NamedFile};
+use actix_web::{App, HttpServer, web};
+
+mod server;
+
+const FRONT_BUILD_DIR: &str = "./front/dist";
+
+#[derive(Debug)]
+pub struct AppState {
+    pub server_pid: Mutex<Option<u32>>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let app_state = web::Data::new(AppState {
+        server_pid: Mutex::new(None),
+    });
+
+    println!("Ready !");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(app_state.clone())
+            .service(
+                web::scope("/api")
+                    .route("/start", web::post().to(server::start_server))
+                    .route("/stop", web::post().to(server::stop_server)),
+            )
+            .service(Files::new("/", FRONT_BUILD_DIR).index_file("index.html"))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
